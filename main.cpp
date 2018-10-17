121d6ae9115f0b3a0eb2a94f988e2c221f57f0cc
Rafael <rafaelmismael@gmail.com>
2018-10-17 11:39:40 -0300

    returning json with all infos, sequential tho

--- main.cpp
+++ main.cpp
@@ -5,16 +5,21 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "json.hpp"
 
+// for convenience
+using json = nlohmann::json;
 using namespace std;
-
-string URL = "https://www.kabum.com.br/produto/96291/computador-gamer-zotac-intel-core-i7-7700-16gb-hd-1tb-ssd-240gb-gtx-1070ti-bluetooth-windows-10-home-g1107tk700b-u";
+bool found = false;
+string URL = "https://www.kabum.com.br/computadores/computador-gamer";
 
 auto r = cpr::Get(cpr::Url{URL});
 
 int counter = 0;
 vector<string> url_list, product_names, product_descriptions, \
               pic_urls, view_prices, financed_priced, product_categories;
+            
+vector<json> product_infos;
 
 static void search_for_links(GumboNode* node) {
   if (node->type != GUMBO_NODE_ELEMENT) {
@@ -27,7 +32,6 @@ static void search_for_links(GumboNode* node) {
     if (href->value[25] == 'p'){
       if(find(url_list.begin(), url_list.end(), (href->value)) == url_list.end()){
         url_list.push_back(href->value);
-        // std::cout << href->value << std::endl;
         counter++;
       }
     }
@@ -39,7 +43,7 @@ static void search_for_links(GumboNode* node) {
   }
 }
 
-static void find_email(GumboNode* node){
+static void find_urls(GumboNode* node){
 	if (node->type == GUMBO_NODE_ELEMENT)
 	{
     GumboAttribute *vAttribute;
@@ -57,25 +61,18 @@ static void find_email(GumboNode* node){
   
       }
 
-			// GumboVector* dt_children = &node->v.element.children;
-			// GumboNode* dt_first_child = static_cast<GumboNode*>(dt_children->data[0]);
-
-			// if (dt_first_child->type == GUMBO_NODE_TEXT && string(dt_first_child->v.text.text).compare("Email Address") == 0)
-			// {
-			// 	printf("FOUND EMAIL ADDRESS");
-			// }
 		}
 
 		GumboVector* children = &node->v.element.children;
 
 		for (unsigned int i = 0; i < children->length; ++i)
 		{
-			find_email(static_cast<GumboNode*>(children->data[i]));
+			find_urls(static_cast<GumboNode*>(children->data[i]));
 		}
 	}
 }
 
-static void find_product_infos(GumboNode* node){
+static json find_product_infos(GumboNode* node, json j){
 	if (node->type == GUMBO_NODE_ELEMENT)
 	{
     GumboAttribute *vAttribute;
@@ -84,12 +81,13 @@ static void find_product_infos(GumboNode* node){
 		if (node->v.element.tag == GUMBO_TAG_H1){
       if (vAttribute = gumbo_get_attribute(&node->v.element.attributes, "itemprop")){
         if (strcmp(vAttribute->value, "name") == 0){
-          product_names.push_back(vAttribute->value);
+          // product_names.push_back(vAttribute->value);
           
           GumboNode* title_text = static_cast<GumboNode *>(node->v.element.children.data[0]);
           if (title_text->type == GUMBO_NODE_TEXT)
           {
-            product_descriptions.push_back(title_text->v.text.text);
+            j["name"] = title_text->v.text.text;
+            product_names.push_back(title_text->v.text.text);
           }
         }
       }
@@ -103,46 +101,102 @@ static void find_product_infos(GumboNode* node){
           GumboNode* title_text = static_cast<GumboNode *>(node->v.element.children.data[0]);
           if (title_text->type == GUMBO_NODE_TEXT)
           {
+            j["description"] = title_text->v.text.text;
             product_descriptions.push_back(title_text->v.text.text);
           }
         }
       }
     }
 
-    //PIC URL
+    //FINANCED PRICE
 		else if (node->v.element.tag == GUMBO_TAG_DIV){
       if (vAttribute = gumbo_get_attribute(&node->v.element.attributes, "class")){
-        cout << vAttribute->value << endl;
-        if (strcmp(vAttribute->value, "img-zoom-container") == 0){
+        if (strcmp(vAttribute->value, "preco_normal") == 0){
+          
+          GumboNode* title_text = static_cast<GumboNode *>(node->v.element.children.data[0]);
+          if (title_text->type == GUMBO_NODE_TEXT)
+          {
+            j["f_price"] = title_text->v.text.text;
 
-          GumboNode* img_url = static_cast<GumboNode *>(node->v.element.children.data[0]);
-          if (img_url->type == GUMBO_NODE_ELEMENT)
-          { 
-            if (vAttribute = gumbo_get_attribute(&node->v.element.attributes, "class")){
-              cout << vAttribute->value << endl;
-            }
-
-            product_descriptions.push_back(img_url->v.text.text);
+            financed_priced.push_back(title_text->v.text.text);
           }
+        }
+      }
+    }
+
+    //VIEW PRICE
+    else if (node->v.element.tag == GUMBO_TAG_SPAN){
+      if (vAttribute = gumbo_get_attribute(&node->v.element.attributes, "itemprop")){
+        if (strcmp(vAttribute->value, "offers") == 0){
+          GumboVector* children = &node->v.element.children;
+          for (unsigned int i = 0; i < children->length; ++i) {
+            GumboNode *node = static_cast<GumboNode*>(children->data[i]);
+            if (node->type == GUMBO_NODE_ELEMENT)
+            {
+              GumboAttribute *vAttribute;
+              if (node->v.element.tag == GUMBO_TAG_STRONG){
+                GumboNode* title_text = static_cast<GumboNode *>(node->v.element.children.data[0]);
+                if (title_text->type == GUMBO_NODE_TEXT)
+                {
+                  j["price"] = title_text->v.text.text;
+
+                  view_prices.push_back(title_text->v.text.text);
+                }
+              }                      
+            }
+          }        
+        }        
+      }
+    }
+
+    //PIC URL
+		else if (node->v.element.tag == GUMBO_TAG_IMG){
+      if (vAttribute = gumbo_get_attribute(&node->v.element.attributes, "itemprop")){
+        // cout << vAttribute->value << endl;
+        if (strcmp(vAttribute->value, "image") == 0){
+          GumboAttribute* url = gumbo_get_attribute(&node->v.element.attributes, "src");
+          string url_value = url->value;
+          if (!found){
+            j["pic_url"] = url_value;
+            
+            found = true;
+          }
+        
         }
       }
     }
 
     GumboVector* children = &node->v.element.children;
     for (unsigned int i = 0; i < children->length; ++i) {
-      find_product_infos(static_cast<GumboNode*>(children->data[i]));
+      j = find_product_infos(static_cast<GumboNode*>(children->data[i]), j);
     }
   }
+  return j;
 }
 
 
 
 int main()
 {
-  // cout << r.text.c_str() << endl;
   GumboOutput *output = gumbo_parse(r.text.c_str());
-  // // Do stuff with output->root
-  // find_email(output->root);
-  find_product_infos(output->root);
+
+  find_urls(output->root);
+  auto pos = URL.find_last_of('/');
+  string category = URL.substr(pos+1);
+
+  for (int i=0; i<url_list.size(); i++){
+    product_categories.push_back(category);
+    auto r = cpr::Get(cpr::Url{url_list[i]});
+    GumboOutput *output = gumbo_parse(r.text.c_str());
+    json product;
+    product = find_product_infos(output->root, product);    
+    cout << product << endl;
+    found = false;
+    // cout << '.';
+    cout.flush();
+  }
+  cout << endl;
+  cout << product_descriptions.size() << " " << product_names.size() << " " << product_categories.size() << " " \
+          << financed_priced.size() << " " << view_prices.size() << " " << pic_urls.size() << endl;
   gumbo_destroy_output(&kGumboDefaultOptions, output);
 }
